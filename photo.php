a7b8cf73099762c73db4ad73f681cb54eba8b89d
prasunmatrix <prasun@matrixnmedia.com>
2022-09-14 18:44:12 +0530

    changes in front end photo

--- photo.php
+++ photo.php
@@ -1,46 +1,54 @@
 <?php
-   //echo "hello..";
-   include_once('header.php');
+//echo "hello..";
+include_once('header.php');
+include_once("conn.php");
+$result = mysqli_query($conn, "SELECT * FROM photo_gallery where is_deleted='0' and status='1' ORDER BY id DESC");
 ?>
-      <!-- End Header -->
-      <main id="main">
-         <!-- ======= Breadcrumbs ======= -->
-         <section class="breadcrumbs">
-            <div class="container">
-               <ol>
-                  <li><a href="https://www.demoyourprojects.com/hzparishad/">Home</a></li>
-                  <li>Photo</li>
-               </ol>
-               <h2>Photo</h2>
-            </div>
-         </section>
-         <!-- End Breadcrumbs -->
-         <section class="inner-page">
-            <div class="container" data-aos="fade-up">
-               <div class="row">
-                  <div class="col-lg-3">
-                     <?php                        
-                        include_once('left-menu.php');
-                     ?>
-                  </div>
-                  <div class="col-lg-9">
-                     <h3>Photo</h3>
-                     <ul class="photo-list">
-                        <li><img src="assets/img/photo-1.jpg" alt=""></li>
-                        <li><img src="assets/img/photo-2.jpg" alt=""></li>
-                        <li><img src="assets/img/photo-3.jpg" alt=""></li>
-                        <li><img src="assets/img/photo-4.jpg" alt=""></li>
-                        <li><img src="assets/img/photo-5.jpg" alt=""></li>
-                        <li><img src="assets/img/photo-6.jpg" alt=""></li>
-                     </ul>
-                  </div>
-               </div>
-               
-            </div>
-         </section>
-      </main>
-      <!-- End #main -->
-      <!-- ======= Footer ======= -->
+<!-- End Header -->
+<main id="main">
+  <!-- ======= Breadcrumbs ======= -->
+  <section class="breadcrumbs">
+    <div class="container">
+      <ol>
+        <li><a href="https://www.demoyourprojects.com/hzparishad/">Home</a></li>
+        <li>Photo</li>
+      </ol>
+      <h2>Photo</h2>
+    </div>
+  </section>
+  <!-- End Breadcrumbs -->
+  <section class="inner-page">
+    <div class="container" data-aos="fade-up">
+      <div class="row">
+        <div class="col-lg-3">
+          <?php
+          include_once('left-menu.php');
+          ?>
+        </div>
+        <div class="col-lg-9">
+          <h3>Photo</h3>
+          <ul class="photo-list">
+            <?php
+              while ($row = mysqli_fetch_array($result)) {
+            ?>
+            <li><img src="assets/uploads/photo_gallery/<?php echo $row['image']; ?>" alt="<?php echo $row['image_title']; ?>"></li>
+            <?php
+              }
+            ?>  
+            <!-- <li><img src="assets/img/photo-2.jpg" alt=""></li>
+            <li><img src="assets/img/photo-3.jpg" alt=""></li>
+            <li><img src="assets/img/photo-4.jpg" alt=""></li>
+            <li><img src="assets/img/photo-5.jpg" alt=""></li>
+            <li><img src="assets/img/photo-6.jpg" alt=""></li> -->
+          </ul>
+        </div>
+      </div>
+
+    </div>
+  </section>
+</main>
+<!-- End #main -->
+<!-- ======= Footer ======= -->
 <?php
-   include_once('footer.php');
+include_once('footer.php');
 ?>
